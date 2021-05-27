c0a65a2271581ede361eabcd31dacd17051800a8
Maya <62715273+violinist1225@users.noreply.github.com>
2021-05-27 13:08:53 -0700

    Survey comp: question is deleted  (#2)
    * designated coder for this component is maya in comment section
    
    * took out 1 question in survey

--- src/Survey.js
+++ src/Survey.js
@@ -1,39 +1,28 @@
 import React,  {useState} from "react";
 import axios from "axios";
 
-// Maya will work on this component 
+// Maya The Bee will work on this survey component 
 export default function Survey(){
- const questions = {
+ const questions = { 
     questions: [
+        
         {
-          name: "question 1",
-          title: "This is the page {pageno} of {pagecount}.",
-          elements: [
-            { type: "text", name:  `What are your skin concerns, i.e.  acne, wrinkles, or dark spots?`, placeHolder:"Provide your answer", isRequired: true}, 
-          ]
-        },
-        {
-            name: "question 2",
+            name: "question 1",
             title: "This is the page {pageno} of {pagecount}.",
             elements: [
               { type: "text", name: "What is your skin type? (Dry, combination,normal, sensitive, or oily) ",  placeHolder:"Provide your answer", isRequired: true}
             ]
         },
-        
-       
-        
         ,
-        
         {
-            name: "question 3",
+            name: "question 2",
             title: "This is the page {pageno} of {pagecount}.",
             elements: [
               { type: "text", name: `Are you pregnant or breast-feeding?`,  placeHolder:"Provide your answer", isRequired: true}
             ]
         },
-        
         {
-            name: "question 4",
+            name: "question 3",
             title: "This is the page {pageno} of {pagecount}.",
             elements: [
               { type: "text", name: "What is your Full name? ",  placeHolder:"Provide your answer", isRequired: true},
